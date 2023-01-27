d94eaa943d6bfd52c73e065f9a2d045fe2e0ca14
root <ogarteetotal@yahoo.com>
2023-01-27 01:41:02 -0800

    adding printf project files

--- _printf.c
+++ _printf.c
@@ -1,24 +1,93 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/**
- * _printf - function my printf
- * @format: string whit format to print
- *
- * Return: number of chars that print
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int length = 0;
 
-	if (format == NULL)
+
+/**
+
+ * _printf - produces output according to a format
+
+ * @format: format string containing the characters and the specifiers
+
+ * Description: this function will call the get_print() function that will
+
+ * determine which printing function to call depending on the conversion
+
+ * specifiers contained into fmt
+
+ * Return: length of the formatted output string
+
+ */
+
+int _printf(const char *format, ...)
+
+{
+
+	int (*pfunc)(va_list, flags_t *);
+
+	const char *p;
+
+	va_list arguments;
+
+	flags_t flags = {0, 0, 0};
+
+
+
+	register int count = 0;
+
+
+
+	va_start(arguments, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+
 		return (-1);
 
-	va_start(args, format);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 
-	length = _print_format(format, args);
-	va_end(args);
-	return (length);
+		return (-1);
+
+	for (p = format; *p; p++)
+
+	{
+
+		if (*p == '%')
+
+		{
+
+			p++;
+
+			if (*p == '%')
+
+			{
+
+				count += _putchar('%');
+
+				continue;
+
+			}
+
+			while (get_flag(*p, &flags))
+
+				p++;
+
+			pfunc = get_print(*p);
+
+			count += (pfunc)
+
+				? pfunc(arguments, &flags)
+
+				: _printf("%%%c", *p);
+
+		} else
+
+			count += _putchar(*p);
+
+	}
+
+	_putchar(-1);
+
+	va_end(arguments);
+
+	return (count);
+
 }
--- main.h
+++ main.h
@@ -1,29 +1,151 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
+
+
+#include <stdlib.h>
+
 #include <stdarg.h>
 
+
+
 /**
-  * struct specifiers - Struct specifiers
-  * @specifier: The conversion specifier
-  * @f: The function pointer
-  */
-typedef struct specifiers
+
+ * struct flags - struct containing flags to "turn on"
+
+ * when a flag specifier is passed to _printf()
+
+ * @plus: flag for the '+' character
+
+ * @space: flag for the ' ' character
+
+ * @hash: flag for the '#' character
+
+ */
+
+typedef struct flags
+
 {
-	char *specifier;
-	int (*f)(va_list args);
-} spc_dt;
 
-int _write(char c);
+	int plus;
+
+	int space;
+
+	int hash;
+
+} flags_t;
+
+
+
+/**
+
+ * struct printHandler - struct to choose the right function depending
+
+ * on the format specifier passed to _printf()
+
+ * @c: format specifier
+
+ * @f: pointer to the correct printing function
+
+ */
+
+typedef struct printHandler
+
+{
+
+	char c;
+
+	int (*f)(va_list ap, flags_t *f);
+
+} ph;
+
+
+
+/* print_nums */
+
+int print_int(va_list l, flags_t *f);
+
+void print_number(int n);
+
+int print_unsigned(va_list l, flags_t *f);
+
+int count_digit(int i);
+
+
+
+/* print_bases */
+
+int print_hex(va_list l, flags_t *f);
+
+int print_hex_big(va_list l, flags_t *f);
+
+int print_binary(va_list l, flags_t *f);
+
+int print_octal(va_list l, flags_t *f);
+
+
+
+/* converter */
+
+char *convert(unsigned long int num, int base, int lowercase);
+
+
+
+/* _printf */
+
 int _printf(const char *format, ...);
-int _print_a_char(va_list args);
-int _print_a_string(va_list args);
-int _print_format(const char *format, va_list args);
-int _print_spec(char format, va_list args);
-int _print_invalid_spec(char prev_format, char format, int count);
-int _print_a_integer(va_list args);
-void _recursion_integer(int a);
-int _print_int_binary(va_list args);
-void _recursion_int_binary(int a);
-int _validate_char(char _type);
 
-#endif /* _MAIN */
+
+
+/* get_print */
+
+int (*get_print(char s))(va_list, flags_t *);
+
+
+
+/* get_flag */
+
+int get_flag(char s, flags_t *f);
+
+
+
+/* print_alpha */
+
+int print_string(va_list l, flags_t *f);
+
+int print_char(va_list l, flags_t *f);
+
+
+
+/* write_funcs */
+
+int _putchar(char c);
+
+int _puts(char *str);
+
+
+
+/* print_custom */
+
+int print_rot13(va_list l, flags_t *f);
+
+int print_rev(va_list l, flags_t *f);
+
+int print_bigS(va_list l, flags_t *f);
+
+
+
+/* print_address */
+
+int print_address(va_list l, flags_t *f);
+
+
+
+/* print_percent */
+
+int print_percent(va_list l, flags_t *f);
+
+
+
+#endif
